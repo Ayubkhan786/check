c2ade973539de7f03f6f3f4bfc8171d67de4edd9
your Name <mohamedabdulayubm@gmail.com>
2022-06-13 12:25:13 +0530

    Adding sample prob

---
+++ Start/Start/Program.cs
@@ -0,0 +1,8 @@
+﻿
+int a = 23;
+int b = 45;
+Console.WriteLine("The given values are :" + a + " , " + b);
+Console.WriteLine("Add the next Value");
+int c = Convert.ToInt16(Console.ReadLine());
+int d = (a + b + c); 
+Console.WriteLine("The total is : " + d );
