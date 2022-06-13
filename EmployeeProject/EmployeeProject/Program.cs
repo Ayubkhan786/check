f097f5bce421c3fd56190be15696790904863ffe
your Name <mohamedabdulayubm@gmail.com>
2022-06-13 15:28:52 +0530

    Emp

---
+++ EmployeeProject/EmployeeProject/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to Employee Wage Computation Program");
+
+int empPresent = 1;
+Random num = new Random();
+int attendance = num.Next(0, 2);
+if (attendance == empPresent)
+{
+
+
+    Console.WriteLine("Is Present");
+
+
+}
+
+else
+{
+    Console.WriteLine("Is Absent");
+}
